115f2feecf7d3359050516afa9e65f98d7f91ab3
Mohammadtalha3 <talhamohammad766@gmail.com>
2023-03-20 00:45:06 +0500

    EDA and Model

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
-import logging
+from src.logger import logging
 
-def error_message_deatil(error,error_detail:set):
+def error_message_deatil(error,error_detail:sys):
     _,_,exc_tb= error_detail.exc_info()
     file_name= exc_tb.tb_frame.f_code.co_filename
     error_message="Error has occured in python script name [{0}] line number [{1}] error message [{2}]".format(
